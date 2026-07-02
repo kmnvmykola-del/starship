ee610d5674bab6c1110b1d20b06a4d0090779221
kmnvmykola-del <kmnv.mykola@gmail.com>
2026-07-02 17:14:29 +0300

    на

--- starship_исправляю/Assets/scripts/Starship/ShipModule.cs
+++ starship_исправляю/Assets/scripts/Starship/ShipModule.cs
@@ -6,7 +6,7 @@ public class ShipModule : NetworkTransform
     [Header("Настройки модуля")]
     public string moduleName;
     public int cost = 100;
-
+    public int health = 100; public int maxHealth = 100;
     // Сколько клеток занимает модуль по X и Y (для больших пушек 2x2 или 3x2)
     public Vector2Int size = new Vector2Int(2, 2);
 
